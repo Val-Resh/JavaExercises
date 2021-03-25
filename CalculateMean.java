fe47e95e77357cb809495bcbc9303b2a971646ca
Val-Resh <80176765+Val-Resh@users.noreply.github.com>
2021-03-25 11:45:52 +0000

    Update CalculateMean.java

--- CalculateMean.java
+++ CalculateMean.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class CalculateMean {
     /*PROBLEM:
     Create a function that takes an array of numbers and returns the mean (average) of all those numbers.
-     */
+    */
 
     public static double mean(int[] nums) {
         double sum = Arrays.stream(nums).sum();
