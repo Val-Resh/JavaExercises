8e94d685f4fc2f7cdcbedd7294109c6cf82f8395
Val-Resh <80176765+Val-Resh@users.noreply.github.com>
2021-05-02 18:11:37 +0100

    Create HowMuchTruth.java

---
+++ HowMuchTruth.java
@@ -0,0 +1,23 @@
+/*PROBLEM:
+Create a function which returns the number of true values there are in an array.
+
+Examples
+countTrue([true, false, false, true, false]) ➞ 2
+countTrue([false, false, false, false]) ➞ 0
+countTrue([]) ➞ 0
+
+Notes
+Return 0 if given an empty array.
+All array items are of the type bool (true or false).*/
+
+import java.util.stream.IntStream;
+
+public class HowMuchTruth {
+	public static int countTrue(boolean[] arr) {
+		int count = 0;
+		for (boolean truth : arr){
+			if (truth == true) count++;
+		}
+		return count;
+	}
+}
